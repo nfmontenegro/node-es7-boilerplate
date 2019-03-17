77232a39dc159b933ba30d0da1fceeff97bbb017
nfmontenegro <nicolas.fmontenegro@gmail.com>
2019-03-17 13:50:24 -0300

    boilerplate node-server

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import express from 'express'
-import puppeteer from 'puppeteer'
+import dotenv from 'dotenv'
 
-// const app = express()
+dotenv.config()
+const app = express()
 
-// app.listen(3000, err => {
-//   if (err) throw new Error(`Error ${err}`)
-//   console.log('Server listening in port: 3000')
-// })
-;(async () => {
-  const browser = await puppeteer.launch({headless: false, slowMo: 150})
-  const page = await browser.newPage()
-  await page.goto('https://www.google.com')
-  await browser.close()
-})()
+const PORT = process.env.PORT || 3000
+app.listen(PORT, err => {
+  if (err) throw new Error(`Crashed ${err}`)
+  console.log(`Server listening in port: http://localhost:${PORT}`)
+})
